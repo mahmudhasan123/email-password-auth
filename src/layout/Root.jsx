4a118e65ee75dd4f20a239918ac122bbb47c931b
mahmudhasan123 <mahmudcodes@gmail.com>
2025-05-01 07:02:17 +0600

    add register form

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
-        
-        <Outlet />
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
