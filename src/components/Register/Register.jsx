4b379cf2d071a2e67d52112b4db53e0281affcaf
mahmudhasan123 <mahmudcodes@gmail.com>
2025-05-01 20:05:41 +0600

    show success and error message

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,43 +1,57 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../../firebase.init";
 
 const Register = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password)
+    
+    setErrorMessage('');
+    setSuccessMessage('');
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log("User Data: ", result);
+        if(result){
+            setSuccessMessage('User created successfully!');
+        }
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+        setErrorMessage(error.message);
+      });
   };
   return (
-    <div>
-      <h2>Create an Account</h2>
-      <form onSubmit={handleRegister}>
+    <div className="max-w-sm mx-auto mt-12">
+      <h2 className="text-2xl font-bold mb-4">Create an Account</h2>
+      <form className="space-y-4" onSubmit={handleRegister}>
         {/* Email field */}
-        <div className="join">
-          <div>
-            <label className="input validator join-item">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              <input type="email" name="email" placeholder="mail@site.com" required />
-            </label>
-            <div className="validator-hint hidden">
-              Enter valid email address
-            </div>
-          </div>
-        </div>
+
+        <label className="input validator join-item">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input type="email" name="email" placeholder="Email" required />
+        </label>
+
         <br />
         {/* Password field */}
         <label className="input validator">
@@ -62,22 +76,19 @@ const Register = () => {
             name="password"
             required
             placeholder="Password"
-            minlength="8"
-            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
           />
         </label>
-        <p className="validator-hint hidden">
-          Must be more than 8 characters, including
-          <br />
-          At least one number <br />
-          At least one lowercase letter <br />
-          At least one uppercase letter
-        </p>
+
         <br />
         {/* Submit button */}
         <input className="btn btn-primary" type="submit" value="Submit" />
       </form>
+      {
+        successMessage && <p className="text-green-500">{successMessage}</p>
+      }
+      {
+        errorMessage && <p className="text-red-500">{errorMessage}</p>
+      }
     </div>
   );
 };
